cc30dbf4a01ea45a9ad2414ea858e4317dcb310e
Marek Zofota <mzofota@gmail.com>
2019-07-03 14:11:41 +0200

    Episode 6

--- routes/web.php
+++ routes/web.php
@@ -11,53 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-	$tasks = [
-		'Go to the store',
-		'Go to the market',
-		'Go to work',
-		'Go to the concert'
-	];
-
-	// OR
-	// return view('home')
-	// 	->withTasks($tasks)
-	// 	->withFoo('foobar');
-
-	// OR
-	// return view('home')
-	// 	->withTasks([
-	// 		'Go to the store',
-	// 		'Go to the market',
-	// 		'Go to work',
-	// 		'Go to the concert'
-	// 	])
-	// 	->withFoo('foobar');
-
-	// OR
-	return view('home')->with([
-		'tasks' => [
-			'Go to the store',
-			'Go to the market',
-			'Go to work',
-			'Go to the concert'
-		],
-		'foo' => 'foobar'
-	]);
-
-	// OR
-    return view('home', [
-    	'tasks' => $tasks,
-    	//'foo' => request('title')		// http://localhost:8000/?title=Laracasts
-    	'foo' => '<script>alert("foobar")</script>'		// javascript
-    	// 'foo' => 'Foobar'
-    ]);
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
